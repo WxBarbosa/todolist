731122fca61346c76cc24a244e028cf1ece83534
wylkson <marcosduval123@gmail.com>
2020-01-15 20:25:45 -0300

    Criação de header/footer/tasklistHome e serviço de listagem de tasks/configuração de bootstrap ng

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AppComponent } from './app.component';
+import { TasklistComponent } from './tasklist/tasklist.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo:'/task', pathMatch: 'full'},
+  { path: 'task', component: TasklistComponent },
+  { path: '**', component: AppComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,33 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+//Bootstrap imports
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+import { TooltipModule } from 'ngx-bootstrap/tooltip';
+import { ModalModule } from 'ngx-bootstrap/modal';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { RecordsService } from './services/records.service';
+import { FooterComponent } from './footer/footer/footer.component';
+import { TasklistComponent } from './tasklist/tasklist.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    TasklistComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserModule,
+    NgbModule
   ],
-  providers: [],
+  exports: [],
+  providers: [RecordsService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
